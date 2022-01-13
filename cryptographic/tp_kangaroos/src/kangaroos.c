df3baf52bf0d9d0ca325e9c61d8e45d1b71f1065
Nicolas Vincent <nicolas.vincent@grenoble-inp.org>
2022-01-13 08:50:23 +0100

    Added comments on kangaroos

--- cryptographic/tp_kangaroos/src/kangaroos.c
+++ cryptographic/tp_kangaroos/src/kangaroos.c
@@ -7,6 +7,35 @@
 
 
 /**
+ * https://ece.uwaterloo.ca/~p24gill/Projects/Cryptography/Pollard's_Rho_and_Lambda/Pollard's_Lambda_Method.html
+ * https://ece.uwaterloo.ca/~p24gill/Projects/Cryptography/Pollard's_Rho_and_Lambda/Project.pdf
+ * https://arxiv.org/pdf/0812.0789.pdf
+ * 
+ * Pollard's Kangaroo (Lambda) method
+ * 
+ * Given g, h ∈ G, solve for x ∈ [a, b] with h = g^x
+ * Here a = 0, b = W = 2^64 - 1 << N order of G
+ *
+ * Preliminaries.
+ * Define a set D \in G of distinguished points.
+ * Define a set S of jump sizes {s_0, ..., s_d}. S = {2^k / 0 <= k <= d}
+ * Choose d such that jump sizes average to µ 
+ * 
+ * 
+ * t_0 = W / 2, w_0 = x := h (i.e. g^x), d_0 = 0. Note that g^w_0 = h.g^d_0.
+ * 
+ * t_{i+1} = t_i + f(g^t_i) := t_i.g^r(e_i). Note that t_i = h.g^b_i 
+ * w_{i+1} = w_i + f(g^w_i) = x + d_{i+1} := w_i.g^r(e_i). Note that w_i = h.g^c_i 
+ * d_{i+1} = d_i + f(h.g^d_i) := ?
+ * 
+ * if g^t_i := t_i \in D, store (g^t_i, t_i - t_0) := (t_i, b_i)
+ * if g^w_i = h.g^d_i := w_i \in D, store (g^w_i, d_i) := (w_i, c_i)
+ * 
+ * hash table : (t_i, total_jumped_distance)
+ * k = random(0, W) 
+ */
+
+/**
  * @brief compute the exponentiation g^x in the field F^{x}_{2^115-85}
  *
  * @param g
