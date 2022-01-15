62f0345bb2fd3396716cedd26c84366f86805d84
Elina Houde <elina.houde@grenoble-inp.org>
2022-01-15 11:44:07 +0100

    Crypto Lab3: dlog64 OK with good execution time

--- cryptographic/tp_kangaroos/includes/kangaroos.h
+++ cryptographic/tp_kangaroos/includes/kangaroos.h
@@ -6,10 +6,15 @@
 
 num128 fast_exp(uint64_t g, uint64_t x);
 num128 gexp(uint64_t x);
-int test_gexp(void);
-
+bool test_gexp(void);
 bool is_equal(num128 result, num128 expected);
 
+void fill_exponents(uint64_t *ej, num128 *gexpej);
+bool test_fill_exponents(void);
+uint64_t get_exponent_for_subset(uint64_t *ej, num128 *gexpej, num128 x, num128 *gej);
+bool is_distinguished(num128 x);
+
 num128 dlog64(num128 target);
+bool test_dlog64(void);
 
 #endif
